2afb73e674453086190a9b4843b87b41bdabf0f5
kingKami <karthikeezhilarasan@gmail.com>
2020-07-05 23:12:45 +0200

    commit save part 2

--- part-2.ps1
+++ part-2.ps1
@@ -4,8 +4,32 @@ function get_computer_info() {
     $computer_info | ConvertTo-Csv -NoTypeInformation | Select-Object -Skip 1 | Set-Content -Path "${csname}.csv"
 }
 
+function fuse_to_inventory() {
+        
+}
+
 function main() {
-    get_computer_info    
+    
+    do {
+        Clear-Host
+        Write-Host "1) get computer specs"
+        Write-Host "2) fuse to inventory"
+        Write-Host "Enter 1 or 2:" -ForegroundColor Yellow -NoNewline
+        $choice = Read-Host
+        
+        if ($choice -eq 1 -or $choice -eq 2) {
+            $ok = $true
+        }
+        else {
+            $ok = $false
+        }
+    } while ($ok -eq $false)
+    
+    
+    switch ($choice) {
+        1 { get_computer_info }
+        2 { fuse_to_inventory }
+    }
 }
 
 main
